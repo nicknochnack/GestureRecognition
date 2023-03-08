fd687422ad04e52964491e642dc3a9d50a0a215e
omaran bazna <omran4d@gmail.com>
2023-03-08 12:46:05 -0500

    -m add the router

---
+++ src/pages/Avatar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Avatar=()=>{
+    return(
+        <>
+        <h1>Avatar</h1>
+        </>
+    )
+}
+export default Avatar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home=()=>{
+
+    return(
+        <>
+        <h1>Welcome to our application</h1>
+        <p>Select what you want to do</p>
+        </>
+    )
+}
+export default Home
